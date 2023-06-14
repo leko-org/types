a988ec017af5986f680dff96c4beb3ff2a2efcad
Eko450-eng <ekrem@wipdesign.de>
2023-06-14 12:18:08 +0200

    Tree shaking

---
+++ .changeset/popular-lions-shout.md
@@ -0,0 +1,5 @@
+---
+"@leko-org/types": minor
+---
+
+Tree shaking
--- schema.ts
+++ schema.ts
@@ -9,7 +9,7 @@ import {
   varchar,
   uniqueIndex,
 } from "drizzle-orm/pg-core";
-import { InferModel, relations } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 
 export const users = pgTable(
   "users",
